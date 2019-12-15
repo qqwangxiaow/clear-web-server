597dc53e8ec6ba285bbd524fc59d8a477eff2cf3
qqwangxiaow <ywwangxiaow@qq.com>
2019-12-15 22:50:31 +0800

    add test client

--- src/core/clear_main.cc
+++ src/core/clear_main.cc
@@ -1,23 +1,32 @@
-#include	"./unp.h"
+#include<sys/socket.h>
 
 int main(int argc, char **argv)
 {
-	int					sockfd;
-	struct sockaddr_in	servaddr;
+	int					listenfd, connfd;
+	pid_t				childpid;
+	socklen_t			clilen;
+	struct sockaddr_in	cliaddr, servaddr;
 
-	if (argc != 2)
-		err_quit("usage: tcpcli <IPaddress>");
-
-	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(SERV_PORT);
-	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = hto ns(SERV_PORT);
 
-	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
-	str_cli(stdin, sockfd);		/* do it all */
+	Listen(listenfd, LISTENQ);
 
-	exit(0);
+	for ( ; ; ) {
+		clilen = sizeof(cliaddr);
+		connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
+
+		if ( (childpid = Fork()) == 0) {	/* child process */
+			Close(listenfd);	/* close listening socket */
+			str_echo(connfd);	/* process the request */
+			exit(0);
+		}
+		Close(connfd);			/* parent closes connected socket */
+	}
 }
