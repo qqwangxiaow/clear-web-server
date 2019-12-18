335dc4a260f955913d1101b5646aa2023e6a4843
qqwangxiaow <ywwangxiaow@qq.com>
2019-12-18 13:04:40 +0800

    basic echo

--- src/core/clear_main.cc
+++ src/core/clear_main.cc
@@ -1,4 +1,32 @@
 #include<sys/socket.h>
+#include<netinet/in.h> /* sockaddr_in{} and other Internet defns */
+#include<unistd.h>	   //fork
+#include<sys/types.h> //fork
+#include<cstring>
+#include<stdlib.h>
+#define	SERV_PORT		 9877
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+using namespace std;
+void
+str_echo(int sockfd)
+{
+	long		arg1, arg2;
+	ssize_t		n;
+	char		line[MAXLINE];
+
+	for ( ; ; ) {
+		if ( (n = Readline(sockfd, line, MAXLINE)) == 0)
+			return;		/* connection closed by other end */
+
+		if (sscanf(line, "%ld%ld", &arg1, &arg2) == 2)
+			snprintf(line, sizeof(line), "%ld\n", arg1 + arg2);
+		else
+			snprintf(line, sizeof(line), "input error\n");
+
+		n = strlen(line);
+		Writen(sockfd, line, n);
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -7,26 +35,31 @@ int main(int argc, char **argv)
 	socklen_t			clilen;
 	struct sockaddr_in	cliaddr, servaddr;
 
-	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+	if(listenfd = socket(AF_INET, SOCK_STREAM, 0))
+		return -1;
 
-	bzero(&servaddr, sizeof(servaddr));
+	memset(&servaddr,0,sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = hto ns(SERV_PORT);
+	servaddr.sin_port        = htons(SERV_PORT);
 
-	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+	if(bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1)
+        return -1;
 
-	Listen(listenfd, LISTENQ);
+	 // 最大等待队列长LISTENQ
+    if(listen(listenfd, LISTENQ) == -1)
+        return -1;
 
 	for ( ; ; ) {
 		clilen = sizeof(cliaddr);
-		connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
+		if(connfd = accept(listenfd, (struct sockaddr*) &cliaddr, &clilen)<0)
+			return -1;
 
-		if ( (childpid = Fork()) == 0) {	/* child process */
-			Close(listenfd);	/* close listening socket */
+		if ( (childpid = fork()) == 0) {	/* child process */
+			close(listenfd);	/* close listening socket */
 			str_echo(connfd);	/* process the request */
 			exit(0);
 		}
-		Close(connfd);			/* parent closes connected socket */
+		close(connfd);			/* parent closes connected socket */
 	}
 }
--- src/core/client.cc
+++ src/core/client.cc
@@ -1,4 +1,16 @@
-#include	"unp.h"
+#include<sys/socket.h>
+#include<netinet/in.h> /* sockaddr_in{} and other Internet defns */
+#include<unistd.h>	   //fork
+#include<sys/types.h> //fork
+#include<cstring>
+#include<stdlib.h>
+#include<iostream>
+#include <netinet/in.h> //inet_pton
+#include<arpa/inet.h>  //inet_pton
+
+#define	SERV_PORT		 9877
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+using namespace std;
 
 int main(int argc, char **argv)
 {
@@ -6,16 +18,16 @@ int main(int argc, char **argv)
 	struct sockaddr_in	servaddr;
 
 	if (argc != 2)
-		err_quit("usage: tcpcli <IPaddress>");
+		cout<<("usage: tcpcli <IPaddress>");
 
-	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERV_PORT);
-	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
-	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+	connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
 	str_cli(stdin, sockfd);		/* do it all */
 
