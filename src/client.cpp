1b5e8bb9ed991811fc30304a19b1c85d7b965efa
qqwangxiaow <ywwangxiaow@qq.com>
2019-12-25 22:31:44 +0800

    write Cmakelists.txt

---
+++ src/client.cpp
@@ -0,0 +1,53 @@
+//
+// Created by 王晓威 on 2019/12/25.
+//
+#include<sys/socket.h>
+#include<netinet/in.h> /* sockaddr_in{} and other Internet defns */
+#include<unistd.h>	   //fork
+#include<sys/types.h> //fork
+#include<cstring>
+#include<stdlib.h>
+#include<iostream>
+#include<netinet/in.h> //inet_pton
+#include<arpa/inet.h>  //inet_pton
+
+#define	SERV_PORT		 9877
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+#define MAXLINE 1024
+using namespace std;
+void str_cli(FILE *fp, int sockfd)
+{
+    char	sendline[MAXLINE], recvline[MAXLINE];
+
+    while (fgets(sendline, MAXLINE, fp) != NULL) {
+
+        write(sockfd, sendline, strlen(sendline));
+
+        if (read(sockfd, recvline, MAXLINE) == 0)
+            cout<<("str_cli: server terminated prematurely");
+
+        fputs(recvline, stdout);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int					sockfd;
+    struct sockaddr_in	servaddr;
+
+    if (argc != 2)
+        cout<<("usage: tcpcli <IPaddress>");
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERV_PORT);
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+    connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    str_cli(stdin, sockfd);		/* do it all */
+
+    exit(0);
+}
---
+++ src/main.cpp
@@ -0,0 +1,69 @@
+//
+// Created by 王晓威 on 2019/12/25.
+//
+#include<sys/socket.h>
+#include<netinet/in.h> /* sockaddr_in{} and other Internet defns */
+#include<unistd.h>	   //fork
+#include<sys/types.h> //fork
+#include<cstring>
+#include<stdlib.h>
+#include<iostream>
+#define	SERV_PORT		 9877
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+#define MAXLINE 1024
+using namespace std;
+void
+Writen(int fd, void *ptr, size_t nbytes)
+{
+    if (write(fd, ptr, nbytes) != nbytes)
+        cout<<("writen error");
+}
+void str_echo(int sockfd)
+{
+    ssize_t		n;
+    char		buf[MAXLINE];
+    again:
+    while ( (n = read(sockfd, buf, MAXLINE)) > 0)
+        Writen(sockfd, buf, n);
+
+    if (n < 0 && errno == EINTR)
+        goto again;
+    else if (n < 0)
+        cout<<("str_echo: read error");
+}
+
+int main(int argc, char **argv)
+{
+    int					listenfd, connfd;
+    pid_t				childpid;
+    socklen_t			clilen;
+    struct sockaddr_in	cliaddr, servaddr;
+    cout<<"goooooooood";
+    if(listenfd = socket(AF_INET, SOCK_STREAM, 0))
+        return -1;
+
+    memset(&servaddr,0,sizeof(servaddr));
+    servaddr.sin_family      = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port        = htons(SERV_PORT);
+
+    bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    // 最大等待队列长LISTENQ
+    if(listen(listenfd, LISTENQ) == -1)
+        return -1;
+
+    for ( ; ; ) {
+        clilen = sizeof(cliaddr);
+        if(connfd = accept(listenfd, (struct sockaddr*) &cliaddr, &clilen)<0)
+            return -1;
+
+        if ( (childpid = fork()) == 0) {	/* child process */
+            close(listenfd);	/* close listening socket */
+            str_echo(connfd);	/* process the request */
+            exit(0);
+        }
+        close(connfd);			/* parent closes connected socket */
+    }
+    cout<<"baaaaaaad";
+}
