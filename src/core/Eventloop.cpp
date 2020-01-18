bf010d0876dd6017f88f1dc3a6fd798ded3c8d76
qqwangxiaow <ywwangxiaow@qq.com>
2020-01-18 23:34:28 +0800

    add eventloop

---
+++ src/core/Eventloop.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 王晓威 on 2020/1/18.
+//
+
+#include "Eventloop.h"
+class Eventloop
+{
+
+};
---
+++ src/core/Eventloop.h
@@ -0,0 +1,8 @@
+//
+// Created by 王晓威 on 2020/1/18.
+//
+
+#ifndef PROJECT_EVENTLOOP_H
+#define PROJECT_EVENTLOOP_H
+
+#endif //PROJECT_EVENTLOOP_H
