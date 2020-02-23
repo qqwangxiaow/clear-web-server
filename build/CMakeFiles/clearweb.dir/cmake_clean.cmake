e0ae0088713f8876301b534b187f91703232ece1
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-24 01:07:08 +0800

    add cmake

---
+++ build/CMakeFiles/clearweb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clearweb.dir/src/core/clear_main.cc.o"
+  "clearweb.pdb"
+  "clearweb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clearweb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
